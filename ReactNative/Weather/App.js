a9bf9639b65585b13948c1b56d78decc7e2efa87
rudans987 <rudans987@naver.com>
2022-12-02 15:12:11 +0900

    style 주는 법

--- ReactNative/Weather/App.js
+++ ReactNative/Weather/App.js
@@ -1,15 +1,19 @@
 //규칙1 div태그 대신 View를 쓰고 container역할이다.
 //규칙2 모든 텍스트는 text component 안에 들어간다.
 //규칙3 스타일을 {{}}로 안에 써도 되고 StyleSheet.create를 써서 밖에 빼도 된다.
-//StyleSheet.create를 쓰면 자동완성기능이 가능하다.
-//statusBar는 ios 혹은 안드로이드 운영체제와 소통해서 상단의 배터리용량 시간을 띄우는 용도이다.
-// contatiner view는 default로 flex container여서 display:flex를 해줄 필요없다
+// StyleSheet.create를 쓰면 자동완성기능이 가능하다.
+// statusBar는 ios 혹은 안드로이드 운영체제와 소통해서 상단의 배터리용량 시간을 띄우는 용도이다.
+// view는 default로 flex container여서 display:flex를 해줄 필요없다
 // flexdirection 기본값이 column이다.
 //레이아웃을 잡을땐 width,height를 쓰지 않고 부모와 자식에 flex를 쓰고 값은 비율로 계산된다.
-//ScrollView 는 style이 아닌 contentContainerStyle라는 이름을 써야 스타일이 먹힌다.
-// Dimensions로 실행되고 있는 핸드폰 스크린의 너비와 높이를 가져올 수 있다.
-// pagingEnabled 스크롤을 페이지로 나눠 어느정도 넘겨야 다음페이지가 나오게한다.
-//showsHorizontalScrollIndicator  수평스크롤 숨길수 있는 props
+// Dimensions로 실행되고 있는 핸드폰 스크린의 너비와 높이를 가져올 수 있는 api
+
+// ScrollView props
+// horizontal : 스크롤을 수평으로 바꾸는 props
+// pagingEnabled : 스크롤을 페이지로 나눠 어느정도 넘겨야 다음페이지가 나오게하는 props
+// showsHorizontalScrollIndicator : 수평스크롤 숨길수 있는 props
+//ScrollView에 스타일 주는 법 : style이 아닌 contentContainerStyle라는 이름을 써야한다.
+
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, StyleSheet, Text, View, Dimensions } from "react-native";
 
